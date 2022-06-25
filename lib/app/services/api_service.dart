d89c6450d110098dd88303a2eb787a91a56d1de2
fatlhr <fatiharahmat257@gmail.com>
2022-06-25 17:25:40 +0700

    Parsing the http response and retrieving the access token

--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:covid19_realtime_project/app/services/api.dart';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 class APIService {
@@ -10,5 +13,16 @@ class APIService {
       api.tokenUri(),
       headers: {'Authorization': 'Basic ${api.apiKey}'},
     );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final accessToken = data['access_token'];
+      if (accessToken != null) {
+        return accessToken;
+      }
+    }
+    debugPrint(
+        'Request ${api.tokenUri()} failed.\nResponse: ${response.statusCode} .');
+    throw response;
   }
 }
