367a502c90c4d0fa6bea42f5a341fb62d9abc841
fatlhr <fatiharahmat257@gmail.com>
2022-06-25 18:59:49 +0700

    Add endpoints to the API class

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -1,5 +1,13 @@
 import 'api_keys.dart';
 
+enum Endpoint {
+  cases,
+  casesSuspected,
+  casesConfirmed,
+  deaths,
+  recovered,
+}
+
 class API {
   final String apiKey;
 
@@ -18,4 +26,20 @@ class API {
       path: 'token',
     );
   }
+
+  Uri endpointUri(Endpoint endpoint) {
+    return Uri(
+      scheme: 'https',
+      host: host,
+      path: _paths[endpoint],
+    );
+  }
+
+  static final Map<Endpoint, String> _paths = {
+    Endpoint.cases: 'cases',
+    Endpoint.casesSuspected: 'casesSuspected',
+    Endpoint.casesConfirmed: 'casesConfirmed',
+    Endpoint.deaths: 'deaths',
+    Endpoint.recovered: 'recovered',
+  };
 }
